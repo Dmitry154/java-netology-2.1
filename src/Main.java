caa6392d5561337499b91beb3ee75e4ad0e0da0d
Ksushka1993 <buslovdn@gmail.com>
2022-06-28 15:50:30 +0700

    Calculation bonus added

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 10_000;
+        int rublesForBonus = 20;
+        int bonus = (ticketPrice / rublesForBonus);
+        System.out.println(bonus);
+    }
 }
